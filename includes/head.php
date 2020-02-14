63dfa27aec1776aa7f293cc49449e4873c1d932e
James Carlo Luchavez <mrstudyfirst@gmail.com>
2020-02-14 18:01:18 +0800

    Initial commit

---
+++ includes/head.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>UMyGee</title>
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="vendors/fontawesome/css/all.min.css">
+  <!-- Bootstrap core CSS -->
+  <link rel="stylesheet" href="vendors/mdbootstrap/css/bootstrap.min.css">
+  <!-- Material Design Bootstrap -->
+  <link rel="stylesheet" href="vendors/mdbootstrap/css/mdb.min.css">
+  <!-- Your custom styles (optional) -->
+  <!-- <link rel="stylesheet" href="css/style.css"> -->
+</head>
---
+++ includes/header.php
@@ -0,0 +1,23 @@
+<header>
+  <!--Navbar-->
+  <nav class="navbar fixed-top navbar-expand-lg navbar-dark red scrolling-navbar">
+    <!-- Navbar brand -->
+    <a class="navbar-brand" href="#"><i class="fas fa-heart fa-fw mx-2" data-fa-transform="rotate-30"></i>Love
+      Mail</a>
+    <!-- Collapse button -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav" aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <!-- Collapsible content -->
+    <div class="collapse navbar-collapse" id="basicExampleNav">
+      <!-- Links -->
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+        <a href="#" class="nav-link" data-toggle="modal" data-target="#modalLRForm">Sign in / Register</a>
+        </li>
+      </ul>
+    </div>
+    <!-- Collapsible content -->
+  </nav>
+  <!--/.Navbar-->
+</header>
