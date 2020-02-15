047f316ef03f8176629d35abf043d8481555bdd6
James Carlo Luchavez <mrstudyfirst@gmail.com>
2020-02-15 16:33:09 +0800

    Changed title

--- includes/head.php
+++ includes/head.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <title>UMyGee</title>
+  <title>Love Mail</title>
   <!-- Font Awesome -->
   <link rel="stylesheet" href="vendors/fontawesome/css/all.min.css">
   <!-- Bootstrap core CSS -->
